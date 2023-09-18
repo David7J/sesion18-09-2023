2f95b9c931988b4f8a6b2b18c02b0e81ceca1a36
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2023-09-18 17:43:14 -0600

    Add files via upload

---
+++ Ejercicios/Arreglos.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Arreglos {
+    
+    public static void main(String [] args){
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Cantidad de notas: ");
+        int cantidadNotas = lector.nextInt();
+
+        while(cantidadNotas <= 0) {
+        System.out.println("No se permiten valores menores o iguales a cero");
+        System.out.println("Cantidad de notas: ");
+        cantidadNotas = lector.nextInt();
+        }
+
+        int[] notas = new int[cantidadNotas];
+        String[] nombres = new String[cantidadNotas];
+        int sumaNotas = 0;
+
+        //Arrays
+        System.out.println("Ingrese las notas del grupo y el nombre de cada uno:");
+        for(int i = 0; i < notas.length ; i++) {
+            System.out.print("Estudiante: [" + (i+1) + "] = ");
+            nombres[i] = lector.nextLine();
+
+            System.out.print("Nota [" + (i+1) + "] = ");
+            notas[i] = lector.nextInt();
+            sumaNotas += notas[i];
+        }
+        for(int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i] + " " + notas[i]);
+        }
+
+        System.out.println("Promedio de notas: " + (double) sumaNotas/(double)cantidadNotas);
+
+
+        lector.close();
+    }
+}
